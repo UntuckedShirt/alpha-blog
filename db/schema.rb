8e2898363fa5c4574f7e2aed3b26511d8370bb54
Nathan Miller <natem31190@gmail.com>
2021-05-12 09:14:04 -0400

    fix this error

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_12_001538) do
+ActiveRecord::Schema.define(version: 2021_05_12_014330) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "description"
   end
 
 end
